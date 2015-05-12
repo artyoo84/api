e2aae7b8d7958038514d44dea773521e7f0cef93
오후 취업반 < @ITbank31-PC>
2015-05-12 17:30:32 +0900

    3day...

---
+++ jse/src/cmm03/flow/No06_Star.java
@@ -0,0 +1,29 @@
+package cmm03.flow;
+
+public class No06_Star {
+	public static void main(String[] args) {
+		int a = 0, b = 0;
+		// ¡Ú
+		for (a = 0; a <= 5; a++) {
+
+			for (b = 0; b <= a; b++) {
+				System.out.print("¡Ú");
+			}
+			System.out.println("");
+		}
+		/*
+		int c = 0, d = 0;
+		for (c = 5; c >= 0; c--) {
+			for (d = 0; d <= c; d++) {
+				System.out.print("¡Ú");
+			}
+			System.out.println("");
+		}
+	*/
+	
+	
+	
+	
+	
+	}
+}
---
+++ jse/src/cmm03/flow/No06_Star2.java
@@ -0,0 +1,18 @@
+package cmm03.flow;
+
+public class No06_Star2 {
+	public static void main(String[] args) {
+		
+		for (int c = 5; c >= 0; c--) {
+			for (int d = 0; d < c; d++) {
+				System.out.print("¡Ú");
+			}
+			System.out.println("");
+		}
+		
+		
+		
+		
+		
+	}
+}
