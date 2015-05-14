4ec3bee8456de018339a9def00143e75a5f7a827
오후 취업반 < @ITbank31-PC>
2015-05-14 21:37:27 +0900

    5 dya.....

---
+++ jse/src/oop01/game/KaupService.java
@@ -0,0 +1,22 @@
+package oop01.game;
+
+import java.util.Scanner;
+
+public class KaupService {
+	public static void main(String[] args) {
+		KaupVO vo = new KaupVO();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("키를 입력하세요 : ");
+		double height = scanner.nextDouble();
+		vo.setHeight(height);
+		System.out.println("몸무게를 입력하세요 : ");
+		double weight = scanner.nextDouble();
+		vo.setWeight(weight);
+		
+		vo.setIdx(); 
+		vo.setMsg("");
+		
+		System.out.println(vo.toString());
+		System.out.println("완료");
+	}
+}
---
+++ jse/src/oop01/game/NumberMatchController.java
@@ -0,0 +1,15 @@
+package oop01.game;
+
+import java.util.Scanner;
+
+public class NumberMatchController {
+	public static void main(String[] args) {
+		NumberMatchService sv = new NumberMatchService();
+		Scanner scanner = new Scanner(System.in);
+		//System.out.println("총 3회만 입력합니다.");
+		System.out.println("1부터 4까지 정수만 입력하세요");
+		int input = scanner.nextInt();
+		sv.start(input);
+		
+	}
+}
