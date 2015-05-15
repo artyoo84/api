7206b26cbf9209941dac65f03ec9cb0ff84914fd
오후 취업반 < @ITbank31-PC>
2015-05-15 21:55:25 +0900

    6 day...

---
+++ jse/src/oop01/encapsule/NumberMatchController.java
@@ -0,0 +1,14 @@
+package oop01.encapsule;
+
+import java.util.Scanner;
+
+public class NumberMatchController {
+	public static void main(String[] args) {
+		NumberMatchService sv = new NumberMatchService();
+		Scanner scanner = new Scanner(System.in);
+		//System.out.println("총 3회만 입력합니다.");
+		System.out.println("1부터 4까지 정수만 입력하세요");
+		int input = scanner.nextInt();
+		sv.start(input);
+	}
+}
---
+++ jse/src/oop01/encapsule/NumberMatchVO.java
@@ -0,0 +1,35 @@
+package oop01.encapsule;
+
+public class NumberMatchVO {
+	private int player, com, count;
+		
+	public int getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(int player) {
+		if(player < 1 || player > 4){
+			System.out.println("1부터 4까지의 정수만 입력해야 합니다.");
+		}else {
+		this.player = player;
+		}
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom() {
+		this.com = (int) ((Math.random()*4)+1);
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+
+}
